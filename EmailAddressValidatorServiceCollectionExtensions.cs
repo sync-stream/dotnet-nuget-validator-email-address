3e62b2ba46d407c8ada3cbdcc2b5bb6d38a6863d
Travis Brown <travis.brown@nidavellir.in>
2022-09-22 15:00:01 -0400

    Adding IServiceCollection extensions with fluid interface

---
+++ EmailAddressValidatorServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SyncStream.Validator.Hostname;
+
+// Define our namespace
+namespace SyncStream.Validator.EmailAddress;
+
+/// <summary>
+/// This class maintains the IServiceCollection extensions for the package
+/// </summary>
+public static class EmailAddressValidatorServiceCollectionExtensions
+{
+    /// <summary>
+    /// This method registers any services or providers to the IServiceCollection
+    /// </summary>
+    /// <param name="instance">The IServiceCollection with which to register everything</param>
+    /// <returns><paramref name="instance" /> for a fluid interface</returns>
+    public static IServiceCollection UseSyncStreamEmailAddressValidator(this IServiceCollection instance) =>
+        instance.UseSyncStreamHostnameValidator();
+}
